9278c91da7c6929337cf3dca52ee04ef5f8af716
Ben Limmer <ben@benlimmer.com>
2016-05-15 09:30:28 -0600

    Change testing framework to mocha. I don't like the QUnit syntax and since I'm about to write a ton of tests, switch over to mocha :-)

--- tests/unit/mixins/confirmation-test.js
+++ tests/unit/mixins/confirmation-test.js
@@ -1,18 +1,26 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+  context,
+  describe,
+  it
+} from 'mocha';
 import Ember from 'ember';
 import ConfirmationMixin from 'ember-onbeforeunload/mixins/confirmation';
-import { module, test } from 'qunit';
 
-module('Unit | Mixin | confirmation');
+describe('ConfirmationMixin', function() {
+  describe('init hook', function() {
+    context('ensure mixed into Route', function() {
+      it('allows mixing into a route', function() {
+        let ConfirmationRoute = Ember.Route.extend(ConfirmationMixin);
+        let subject = ConfirmationRoute.create();
+        expect(subject).to.be.ok;
+      });
 
-test('allows mixing into a route', function(assert) {
-  let ConfirmationRoute = Ember.Route.extend(ConfirmationMixin);
-  let subject = ConfirmationRoute.create();
-  assert.ok(subject);
-});
-
-test('throws an exception when mixing into a plain-ol\' ember object', function(assert) {
-  let ConfirmationObject = Ember.Object.extend(ConfirmationMixin);
-  assert.throws(function() {
-    ConfirmationObject.create();
+      it('throws an exception when mixing into a plain-ol\' ember object', function() {
+        let ConfirmationObject = Ember.Object.extend(ConfirmationMixin);
+        expect(ConfirmationObject.create.bind()).to.throw;
+      });
+    });
   });
 });
